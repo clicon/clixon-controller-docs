fa172d5e2338bf0985a1d4101765eb9904d279cd
Olof hagsand <olof@hagsand.se>
2023-08-07 11:34:45 +0200

    try sphinx-rtd-theme

--- conf.py
+++ conf.py
@@ -40,7 +40,10 @@ highlight_language = 'sh'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx_rtd_theme']
+
+html_theme = "sphinx_rtd_theme"
+#html_theme_path = ["_themes", ]
 #extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
