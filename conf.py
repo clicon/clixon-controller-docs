b7d15fe8d6f398a67d24d24aa3db18921d0499cf
Olof hagsand <olof@hagsand.se>
2025-01-30 11:12:40 +0100

    Controller 1.3.0

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2'
+release = '1.3'
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
